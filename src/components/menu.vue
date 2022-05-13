4e90259636de3f8d2acb1878ae167d7c243bb712
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-13 10:58:46 +0200

    base menu

---
+++ src/components/menu.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-black flex justify-between p-3">
+    <users></users>
+    <home></home>
+    <message></message>
+  </div>
+</template>
+
+<script >
+import users from "./icons/users.vue";
+import home from "./icons/home.vue";
+import message from "./icons/message.vue";
+
+export default {
+  name: "App",
+  components: { users, home, message },
+};
+</script>
--- src/views/GuideView.vue
+++ src/views/GuideView.vue
@@ -1,14 +1,16 @@
 <template>
   <Header></Header>
   <Footer></Footer>
+  <Menu></Menu>
 </template>
 
 <script >
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
+import Menu from "../components/menu.vue";
 
 export default {
   name: "App",
-  components: { Header, Footer },
+  components: { Header, Footer, Menu },
 };
 </script>
