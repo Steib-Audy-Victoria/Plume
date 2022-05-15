d5cd4eae68f77474afe82f5db343222f43afbb74
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-15 18:15:07 +0200

    post feed base

---
+++ src/components/PostFeed.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-max border-2 border-red-600 rounded-xl p-3">
+    <div class="rounded-3xl bg-white"></div>
+    <img src="public/img/monster.jpg" class="object-cover rounded-t-lg pb-3" />
+    <div class="flex justify-between">
+      <p class="text-xl font-semibold text-left text-black">Clément B.</p>
+      <Heart></Heart>
+    </div>
+  </div>
+</template>
+
+<script >
+import Heart from "./icons/Heart.vue";
+
+export default {
+  name: "App",
+  components: { Heart },
+};
+</script>
--- src/views/GuideView.vue
+++ src/views/GuideView.vue
@@ -3,6 +3,7 @@
   <Footer></Footer>
   <Menu></Menu>
   <CardArtisteRecherche></CardArtisteRecherche>
+  <PostFeed></PostFeed>
 </template>
 
 <script >
@@ -10,9 +11,10 @@ import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
 import Menu from "../components/menu.vue";
 import CardArtisteRecherche from "../components/CardArtisteRecherche.vue";
+import PostFeed from "../components/PostFeed.vue";
 
 export default {
   name: "App",
-  components: { Header, Footer, Menu, CardArtisteRecherche },
+  components: { Header, Footer, Menu, CardArtisteRecherche, PostFeed },
 };
 </script>
