feca4e6efa624b2cac77cb24b1638459e5daf888
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-17 14:41:32 +0200

    correction home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,8 @@
-<template>
-  <Menu></Menu>
-  <CardArtisteRecherche></CardArtisteRecherche>
-  <PostFeed></PostFeed>
-  <CardCommande></CardCommande>
-</template>
+<template></template>
 
 <script >
-import Menu from "../components/menu.vue";
-import CardArtisteRecherche from "../components/CardArtisteRecherche.vue";
-import PostFeed from "../components/PostFeed.vue";
-import CardCommande from "../components/CardCommande.vue";
-
 export default {
   name: "App",
-  components: {
-    Menu,
-    CardArtisteRecherche,
-    PostFeed,
-    CardCommande,
-  },
+  components: {},
 };
 </script>
