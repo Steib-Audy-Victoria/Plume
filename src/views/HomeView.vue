46f0077d8dc1757284f79d44ab661f9cb5ff845c
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-17 21:45:29 +0200

    Accueil

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,18 @@
   <div class="flex justify-center">
     <h1 class="font-praise text-11xl">Plume</h1>
   </div>
+  <div>
+    <p class="font-baloo text-6xl w-1/5">NOUVELLE COMMANDE</p>
+    <hr class="my-2 border-1 border-Rouge w-1/3" />
+  </div>
+  <div>
+    <p class="font-baloo text-6xl w-1/5">COMMANDE EN COURS</p>
+    <hr class="my-2 border-1 border-Rouge w-1/3" />
+  </div>
+  <div>
+    <p class="font-baloo text-6xl w-1/5">COMMANDES PASSÉES</p>
+    <hr class="my-2 border-1 border-Rouge w-1/3" />
+  </div>
 </template>
 
 <script >
