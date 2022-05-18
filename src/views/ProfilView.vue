9509573c0e57ca5d1b09787d4b01826d50be53a5
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-18 14:21:31 +0200

    img profil

--- src/views/ProfilView.vue
+++ src/views/ProfilView.vue
@@ -1,15 +1,6 @@
 <template>
-  <div class="profil">
-    <h1>Ceci est la page profil</h1>
+  <div>
+    <img src="" alt="" />
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .apropos {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
