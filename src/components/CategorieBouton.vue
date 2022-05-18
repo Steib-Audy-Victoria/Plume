e4c970638fd005ba81eeb734146cf6a113c94e80
louplobato <louplelpro@gmail.com>
2022-05-18 15:23:08 +0200

    début profil

--- src/components/CategorieBouton.vue
+++ src/components/CategorieBouton.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="w-max p-1 h-24px rounded-xl border-2 border-red-600 font-baloo">
-        <p>
+    <div class="w-max p-1 h-24px rounded-3xl border-2 border-red-600 font-baloo font-semibold">
+        <p class="font-baloo font-semibold px-3">
             Manga
         </p>
     </div>
--- src/views/ProfilView.vue
+++ src/views/ProfilView.vue
@@ -1,6 +1,30 @@
 <template>
-  <div>
-    <img src="" alt="" />
+  <div class="pt-16">
+    <div>
+      <img src="/pro" alt="">
+    <div class="flex">
+      <settings></settings>
+      <p>
+        Laura_mg0
+      </p>
+      <edit></edit>
+    </div>  
+    </div>
+
+
   </div>
+
 </template>
+
+<script>
+ import settings from '../components/icons/settings.vue'
+ import edit from '../components/icons/edit.vue'
+
+ export default {
+   components : {
+     settings,
+     edit
+   }
+ }
+</script>
 
