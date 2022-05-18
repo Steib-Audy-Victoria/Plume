ab3332ce3dd6c3ed7088cd22082d6d5867b5f4d3
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-18 15:04:03 +0200

    nous soutenir + a propos base

--- src/views/AproposView.vue
+++ src/views/AproposView.vue
@@ -1,15 +1,27 @@
 <template>
-  <div class="apropos">
-    <h1>Ceci est la page à propos</h1>
+  <div class="flex items-center justify-center pt-16 mb-5">
+    <PlumeB></PlumeB>
+    <h2 class="text-3xl font-baloo font-bold pl-5">À Propos</h2>
+  </div>
+  <div class="flex items-center justify-center">
+    <p class="pl-2 font-baloo text-base w-3/4">
+      Plume est un projet universitaire dirigé par nous-même, Loup Lobato,
+      Antonin Heuguet et Victoria Steib--Audy. Nous sommes des étudiants en
+      première année de BUT Métiers du Multimédia et de l’Internet à
+      Montbéliard. Nous avons récupéré le projet de base et l’avons modifié
+      selon notre vision de la chose. Le projet s’est donc nommé “Plume”.
+    </p>
+  </div>
+  <div>
+    <h3 class="font-baloo font-bold text-xl pl-6 my-8">Qui sommes nous ?</h3>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .apropos {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script>
+import PlumeB from "../components/icons/PlumeB.vue";
+
+export default {
+  name: "App",
+  components: { PlumeB },
+};
+</script>
