3e3d0d5f8ed91ee616a93d5c89e9a70a2fecf9a4
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-19 16:50:51 +0200

    connextion et inscription

---
+++ src/components/BoutonConnexion.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="
+      flex
+      justify-center
+      border-2 border-white
+      bg-transparent
+      w-80
+      px-4
+      rounded-md
+      m-2
+    "
+  >
+    <img class="pr-8" src="public/img/google.png" alt="" />
+    <p class="font-baloo text-white text-lg">Continuer avec Google</p>
+  </div>
+</template>
--- src/views/InscriptionView.vue
+++ src/views/InscriptionView.vue
@@ -1,15 +1,73 @@
 <template>
-  <div class="inscription">
-    <h1>Ceci est la page inscription</h1>
+  <div class="pt-16 bg-Pourpre">
+    <h1 class="font-praise text-5xl text-white text-center">Plume</h1>
+    <div class="flex flex-col place-items-center my-10">
+      <BoutonConnexion></BoutonConnexion>
+      <BoutonConnexion></BoutonConnexion>
+      <BoutonConnexion></BoutonConnexion>
+    </div>
+    <div class="flex justify-center items-center">
+      <hr class="my-1 pb-2 border-3 border-white w-1/4" />
+      <p class="text-white font-baloo text-xl px-2">OU</p>
+      <hr class="my-1 pb-2 border-3 border-white w-1/4" />
+    </div>
+    <div class="flex flex-col place-items-center">
+      <p class="text-white font-baloo text-lg px-2">NOM</p>
+      <input
+        class="w-1/2 border-Rouge border-2 rounded-lg"
+        type="text"
+        placeholder="Insérez du texte"
+      />
+    </div>
+    <div class="flex flex-col place-items-center">
+      <p class="text-white font-baloo text-lg px-2">PRÉNOM</p>
+      <input
+        class="w-1/2 border-Rouge border-2 rounded-lg"
+        type="text"
+        placeholder="Insérez du texte"
+      />
+    </div>
+    <div class="flex flex-col place-items-center">
+      <p class="text-white font-baloo text-lg px-2">EMAIL</p>
+      <input
+        class="w-1/2 border-Rouge border-2 rounded-lg"
+        type="text"
+        placeholder="Insérez du texte"
+      />
+    </div>
+    <div class="flex flex-col place-items-center">
+      <p class="text-white font-baloo text-lg px-2">PSEUDO</p>
+      <input
+        class="w-1/2 border-Rouge border-2 rounded-lg"
+        type="text"
+        placeholder="Insérez du texte"
+      />
+    </div>
+    <div class="flex flex-col place-items-center">
+      <p class="text-white font-baloo text-lg px-2">MOT DE PASSE</p>
+      <input
+        class="w-1/2 border-Rouge border-2 rounded-lg"
+        type="text"
+        placeholder="Insérez du texte"
+      />
+    </div>
+    bouton bouton
+    <div class="flex justify-center items-center">
+      <hr class="my-1 pb-2 border-3 border-white w-1/4" />
+      <p class="text-white font-baloo text-xl px-2">OU</p>
+      <hr class="my-1 pb-2 border-3 border-white w-1/4" />
+    </div>
+    bouton
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .apropos {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script >
+import BoutonConnexion from "../components/BoutonConnexion.vue";
+
+export default {
+  name: "App",
+  components: {
+    BoutonConnexion,
+  },
+};
+</script>
