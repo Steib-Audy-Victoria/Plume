0acf047a67388a51e862ec9476eb9884fd2bbc96
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-21 20:59:06 +0200

    Nouvelle Commande

---
+++ src/components/Btn.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-gradient-to-r to-Orange from-Rouge w-max rounded-xl p-0.5 m-6">
+    <button
+      class="text-xl bg-white rounded-xl font-semibold font-baloo w-64 h-12"
+    >
+      {{ MsgBtn }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    MsgBtn: String,
+  },
+};
+</script>
--- src/views/NouvelleCommandeView.vue
+++ src/views/NouvelleCommandeView.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="nouvellecommande">
-    <h1>Ceci est la page nouvelle commande</h1>
+  <div class="pt-16 flex flex-col place-items-center">
+    <p class="font-baloo text-xl font-semibold p-3">Importez un dessin</p>
+    <btn-ajout class="m-5"></btn-ajout>
+    <p class="font-baloo text-xl font-semibold p-3">Nommez-le</p>
+    <div class="bg-gradient-to-r to-Orange from-Rouge rounded-lg p-0.5">
+      <input
+        class="w-80 rounded-lg placeholder:pl-2"
+        type="text"
+        placeholder="Insérez du texte"
+      />
+    </div>
+    <p class="font-baloo text-xl font-semibold p-3">Quel style ?</p>
+    <div class="grid sm:grid-cols-3 grid-cols-2 place-items-center m-3">
+      <CategorieBouton CatBtn="Manga"></CategorieBouton>
+      <CategorieBouton CatBtn="Comics"></CategorieBouton>
+      <CategorieBouton CatBtn="Réaliste"></CategorieBouton>
+      <CategorieBouton CatBtn="BD"></CategorieBouton>
+      <CategorieBouton CatBtn="3D"></CategorieBouton>
+      <CategorieBouton CatBtn="Caricature"></CategorieBouton>
+      <CategorieBouton CatBtn="Peinture"></CategorieBouton>
+      <CategorieBouton CatBtn="Aquarelle"></CategorieBouton>
+    </div>
+    <Btn MsgBtn="Commencer"></Btn>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .apropos {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script>
+import BtnAjout from "../components/BtnAjout.vue";
+import CategorieBouton from "../components/CategorieBouton.vue";
+import Btn from "../components/Btn.vue";
+
+export default {
+  components: { BtnAjout, CategorieBouton, Btn },
+};
+</script>
