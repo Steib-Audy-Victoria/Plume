b170dab35a944a316bcae6a408b959e1b118271e
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-05-22 10:45:39 +0200

    Profil

--- src/views/ProfilView.vue
+++ src/views/ProfilView.vue
@@ -1,30 +1,41 @@
 <template>
   <div class="pt-16">
-    <div>
-      <img src="/pro" alt="">
-    <div class="flex">
-      <settings></settings>
-      <p>
-        Laura_mg0t
-      </p>
-      <edit></edit>
-    </div>  
+    <div class="mb-10">
+      <div class="flex justify-center">
+        <img src="public/img/img_profil.jpg" alt="" />
+      </div>
+      <div class="flex justify-center mt-2">
+        <settings></settings>
+        <p class="font-baloo font-semibold text-xl text-center px-5">
+          Laura_mg0t
+        </p>
+        <edit></edit>
+      </div>
     </div>
-
-
+    <div class="grid grid-cols-2">
+      <div class="flex justify-center">
+        <imageD></imageD>
+      </div>
+      <div class="flex justify-center">
+        <heartB></heartB>
+      </div>
+    </div>
   </div>
-
 </template>
 
 <script>
- import settings from '../components/icons/settings.vue'
- import edit from '../components/icons/edit.vue'
+import settings from "../components/icons/settings.vue";
+import edit from "../components/icons/edit.vue";
+import imageD from "../components/icons/imageD.vue";
+import heartB from "../components/icons/heartB.vue";
 
- export default {
-   components : {
-     settings,
-     edit
-   }
- }
+export default {
+  components: {
+    settings,
+    edit,
+    imageD,
+    heartB,
+  },
+};
 </script>
 
