3f9f1ad7f24924a0ffc77996514915ec65cf6da5
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-06-01 22:18:35 +0200

    Feed Firebase

--- src/components/PostFeed.vue
+++ src/components/PostFeed.vue
@@ -2,9 +2,9 @@
   <div class="bg-gradient-to-r to-Orange from-Rouge p-0.5 w-max rounded-xl m-3">
     <div class="bg-white p-0.5 w-max rounded-xl">
       <div class="rounded-3xl bg-white"></div>
-      <img src="public/img/monster.jpg" class="object-cover p-3" />
+      <img :src="image" class="object-cover p-3" />
       <div class="flex justify-between p-1">
-        <p class="text-xl font-semibold text-left text-black">Clément B.</p>
+        <p class="text-xl font-semibold text-left text-black">{{ nom }}</p>
         <Heart></Heart>
       </div>
     </div>
@@ -17,5 +17,9 @@ import Heart from "./icons/Heart.vue";
 export default {
   name: "App",
   components: { Heart },
+  props: {
+    image: String,
+    nom: String,
+  },
 };
 </script>
--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -1,27 +1,80 @@
 <template>
-  <div class="pt-16"></div>
-  <div
-    class="
-      flex flex-col
-      md:grid md:grid-cols-2
-      xl:grid-cols-3
-      place-items-center
-    "
-  >
-    <PostFeed></PostFeed>
-    <PostFeed></PostFeed>
-    <PostFeed></PostFeed>
-    <PostFeed></PostFeed>
-  </div>
+  <main>
+    <div class="pt-16"></div>
+    <div
+      class="
+        flex flex-col
+        md:grid md:grid-cols-2
+        xl:grid-cols-3
+        place-items-center
+      "
+    >
+      <PostFeed
+        v-for="dessins in listeDessins"
+        :key="dessins.id"
+        :image="dessins.image"
+        :nom="dessins.Nom"
+      ></PostFeed>
+      <!--<PostFeed></PostFeed>
+      <PostFeed></PostFeed>
+      <PostFeed></PostFeed>-->
+    </div>
+  </main>
 </template>
 
 <script>
 import PostFeed from "../components/PostFeed.vue";
 
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
+import {
+  getStorage,
+  ref,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-storage.js";
+
 export default {
   name: "App",
   components: {
     PostFeed,
+  },
+  data() {
+    return {
+      listeDessins: [],
+      nom: null,
+    };
+  },
+  mounted() {
+    this.getDessins();
+  },
+  methods: {
+    async getDessins() {
+      const firestore = getFirestore();
+      const dbDessins = collection(firestore, "dessins");
+      const query = await onSnapshot(dbDessins, (snapshot) => {
+        console.log("query", query);
+        this.listeDessins = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        this.listeDessins.forEach(function (personne) {
+          const storage = getStorage();
+          const spaceRef = ref(storage, "dessins/" + personne.image);
+          getDownloadURL(spaceRef)
+            .then((url) => {
+              personne.image = url;
+              console.log("personne", personne);
+            })
+            .catch((error) => {
+              console.log("erreur downloadUrl", error);
+            });
+        });
+        console.log("listeDessins", this.listeDessins);
+      });
+    },
   },
 };
 </script>
