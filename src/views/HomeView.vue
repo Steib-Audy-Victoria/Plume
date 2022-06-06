4e4cec50cb43abb4669055d26aa8ca3c6781081b
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-06-06 21:44:36 +0200

    changement page d'accueil connecter déconnecter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,7 +16,7 @@
       Plume
     </h1>
   </div>
-  <div class="absolute top-14 left-48">
+  <div v-if="users == null" class="flex justify-center items-center m-4">
     <button
       class="
         bg-Pourpre
@@ -31,41 +31,43 @@
       <RouterLink to="/Connexion">Se connecter</RouterLink>
     </button>
   </div>
-  <div class="pb-8">
-    <div>
-      <p class="font-baloo text-4xl w-1/5">NOUVELLE COMMANDE</p>
+  <div v-else>
+    <div class="pb-8">
+      <div>
+        <p class="font-baloo text-4xl w-1/5">NOUVELLE COMMANDE</p>
+        <hr class="my-2 border-1 border-gradient-r-ligne w-1/3" />
+      </div>
+      <div class="flex justify-center">
+        <RouterLink to="/NouvelleCommande"><BtnAjout></BtnAjout></RouterLink>
+      </div>
+    </div>
+    <div class="pb-8">
+      <p class="font-baloo text-4xl w-1/5">COMMANDE EN COURS</p>
       <hr class="my-2 border-1 border-gradient-r-ligne w-1/3" />
+      <div
+        class="
+          flex flex-col
+          md:grid md:grid-cols-2
+          xl:grid-cols-3
+          place-items-center
+        "
+      >
+        <CardCommande></CardCommande>
+      </div>
     </div>
-    <div class="flex justify-center">
-      <RouterLink to="/NouvelleCommande"><BtnAjout></BtnAjout></RouterLink>
-    </div>
-  </div>
-  <div class="pb-8">
-    <p class="font-baloo text-4xl w-1/5">COMMANDE EN COURS</p>
-    <hr class="my-2 border-1 border-gradient-r-ligne w-1/3" />
-    <div
-      class="
-        flex flex-col
-        md:grid md:grid-cols-2
-        xl:grid-cols-3
-        place-items-center
-      "
-    >
-      <CardCommande></CardCommande>
-    </div>
-  </div>
-  <div class="pb-8">
-    <p class="font-baloo text-4xl w-1/5">COMMANDES PASSÉES</p>
-    <hr class="my-2 border-1 border-gradient-r-ligne w-1/3" />
-    <div
-      class="
-        flex flex-col
-        md:grid md:grid-cols-2
-        xl:grid-cols-3
-        place-items-center
-      "
-    >
-      <CardCommande2></CardCommande2>
+    <div class="pb-8">
+      <p class="font-baloo text-4xl w-1/5">COMMANDES PASSÉES</p>
+      <hr class="my-2 border-1 border-gradient-r-ligne w-1/3" />
+      <div
+        class="
+          flex flex-col
+          md:grid md:grid-cols-2
+          xl:grid-cols-3
+          place-items-center
+        "
+      >
+        <CardCommande2></CardCommande2>
+      </div>
     </div>
   </div>
 </template>
@@ -75,8 +77,30 @@ import CardCommande from "../components/CardCommande.vue";
 import CardCommande2 from "../components/CardCommande2.vue";
 import BtnAjout from "../components/BtnAjout.vue";
 
+// Fonction authentification
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js";
+
 export default {
   name: "App",
   components: { CardCommande, CardCommande2, BtnAjout },
+  data() {
+    return {
+      users: null,
+    };
+  },
+  mounted() {
+    this.getUsers();
+  },
+  methods: {
+    async getUsers() {
+      await getAuth().onAuthStateChanged(
+        function (users) {
+          if (users) {
+            this.users = users;
+          }
+        }.bind(this)
+      );
+    },
+  },
 };
 </script>
