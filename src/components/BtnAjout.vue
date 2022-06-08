df03b5a017ff09cdf06b3376ff61c47d02c97972
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-06-08 10:32:34 +0200

    hover sur btn ajout dessin

--- src/components/BtnAjout.vue
+++ src/components/BtnAjout.vue
@@ -1,19 +1,22 @@
 <template>
-  <button
-    class="
-      text-xl text-white
-      rounded-xl
-      font-semiboldfont-baloo
-      w-64
-      h-12
-      drop-shadow-2xl
-      bg-gradient-to-l
-      to-Rouge
-      from-Orange
-    "
-  >
-    +
-  </button>
+  <div class="bg-gradient-to-l to-Rouge from-Orange p-0.5 w-max rounded-xl m-3">
+    <button
+      class="
+        text-xl text-white
+        rounded-xl
+        font-semiboldfont-baloo
+        w-64
+        h-12
+        drop-shadow-2xl
+        bg-gradient-to-l
+        to-Rouge
+        from-Orange
+        hover:from-white hover:to-white hover:text-black
+      "
+    >
+      +
+    </button>
+  </div>
 </template>
 
 <script>
